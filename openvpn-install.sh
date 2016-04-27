f84e74c451f596e7930d27fc00ec7455d3f9fd8c
DevMega <csong@devmega.com>
2016-04-27 09:25:30 -0500

    Update openvpn-install.sh

--- openvpn-install.sh
+++ openvpn-install.sh
@@ -206,7 +206,7 @@ else
 	echo "   7) DNS.Watch"
 	echo "   8) You specify the DNS server IP-s"
 	read -p "DNS [1-7]: " -e -i 2 DNS
-	if [[ "$DNS" = '7' ]]; then
+	if [[ "$DNS" = '8' ]]; then
                 echo "Please leave a space between DNS IP entries"
 		echo "Example: 8.8.8.8 8.8.4.4 129.250.35.250"
                 read -e -p "Specify the DNS server IP-s. Space is the separator: " OWNDNS
